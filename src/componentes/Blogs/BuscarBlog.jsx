60f2d9c45cf1889fe4d67f4d8a2f7bcd3efb2751
A113E <amg11amg2@gmail.com>
2025-09-14 17:14:18 -0400

    Pruebas de BuscarBlog

--- src/componentes/Blogs/BuscarBlog.jsx
+++ src/componentes/Blogs/BuscarBlog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BuscarBlog = ({ buscarBlog, handleBusquedaChange }) => (
   <div>
-    <strong>Buscar título del blog:</strong> <input value={buscarBlog} onChange={handleBusquedaChange} />
+    <strong>Buscar título del blog:</strong> <input value={buscarBlog} onChange={handleBusquedaChange} placeholder='Titulo del blog'/>
   </div>
 )
 
---
+++ src/componentes/Blogs/BuscarBlog.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BuscarBlog from './BuscarBlog.jsx'
+
+// Prueba que verifica que se renderiza el input con el valor incial
+test('renderiza el input con el valor inicial', () => {
+    const mockHandler = jest.fn() // Controlador de eventos
+    render(<BuscarBlog buscarBlog={'React'} handleBusquedaChange={mockHandler} />)
+
+    const input = screen.getByPlaceholderText('Titulo del blog')
+    expect(input.value).toBe('React') // Comprueba que se renderiza el valor
+})
+
+// Prueba que verifica que se llame al controlador de eventos cuando el usuario escribe en el input
+test('llama al controlador de eventos cuando el usuario escribe en el input', async () => {
+    const mockHandler = jest.fn() // Controlador de eventos
+    const usuario = userEvent.setup() // Usuario
+    render(<BuscarBlog buscarBlog={''} handleBusquedaChange={mockHandler} />)
+
+    const input = screen.getByPlaceholderText('Titulo del blog')
+
+    await usuario.type(input, 'Probando buscador...')
+
+    expect(mockHandler).toHaveBeenCalled() // Comprueba que se llama al controlador
+})
